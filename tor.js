5a93c8d5d03ed10cb5bb2313d62f3c98933c0a4d
adrianaleticya <leticya.gontijo@gmail.com>
2020-06-02 10:05:09 -0300

    Updating variable name

--- tor.js
+++ tor.js
@@ -2,45 +2,50 @@ const request = require('request')
 const cheerio = require('cheerio')
 const fs = require('fs')
 
-const [PAGINA_INICIAL, PAGINA_FINAL] = [0, 60]
+const [firstPage, finalPage] = [0, 60]
 
-if(!PAGINA_INICIAL) fs.appendFile('tor.csv', 'TimeStamp;Date;Title;Uri;Author;Tags\n', () => {})
+const fileName = 'tor.csv'
 
-const baseUri = 'https://blog.torproject.org'
+if(!firstPage) fs.appendFile(fileName, 'TimeStamp;Date;Title;Uri;Author;Tags\n', () => {})
 
-for(let i = PAGINA_INICIAL; i < PAGINA_FINAL; i++) {
-  const mainUri = `${baseUri}/?page=${i}`
+const baseURL = 'https://blog.torproject.org'
 
-  request(mainUri, function(err, res, body) {
-    if(err) console.log(`Erro: ${err}`)
+for(let page = firstPage; page < finalPage; page++) {
+  const mainURL = `${baseURL}/?page=${page}`
+
+  request(mainURL, function(err, res, body) {
+    if(err) console.log(`Error: ${err}`)
   
     const $ =  cheerio.load(body)
-    const qs = '.main-content-container .main-content .main-content-inner .inner-inner .region-content .views-element-container .views-row article'
+    const querrySelector = '.main-content-container .main-content .main-content-inner .inner-inner .region-content .views-element-container .views-row article'
   
-    $(qs).each(function() {
-      const articleUri = `${baseUri}${$(this).find('.title a').attr('href')}`
-      request(articleUri, function(err, res, body) {
-        if(err) console.log(`Erro: ${err}`)
+    $(querrySelector).each(function() {
+      const articleURL = `${baseURL}${$(this).find('.title a').attr('href')}`
+      request(articleURL, function(err, res, body) {
+        if(err) console.log(`Error: ${err}`)
   
         const $ = cheerio.load(body)
-        const qs = '.main-content-container .main-content .main-content-inner .inner-inner .region-content article'
-        const article = $(qs).first()
+        const querrySelector = '.main-content-container .main-content .main-content-inner .inner-inner .region-content article'
+        const article = $(querrySelector).first()
         
         const articleTimeStamp = article.find('.author span').attr('content').split('+')[0]
         const articleDate = article.find('.author span').text().trim()
         const articleTitle = article.find('.title span').text().trim()
         const articleAuthor = article.find('.author a').text().trim()
         const articleTags = []
+
         article.find('.field--name-field-tags .field--items .field--item a').each(function(i, el) {
           articleTags[i] = $(this).text()
         })
         
-	const content = `${articleTimeStamp};${articleDate};${articleTitle};${articleUri};${articleAuthor};${articleTags.join(';')}\n`
+        const content = `${articleTimeStamp};${articleDate};${articleTitle};${articleURL};${articleAuthor};${articleTags.join(';')}\n`
+        
         console.log(content)
-        fs.appendFile('tor.csv', content, () => {})
-
-      })
-  
+        
+        fs.appendFile(fileName, content, () => {})
+      })  
     })
+
   })
+
 }

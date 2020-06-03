2e3dae8b6922c38e340d0bb544986a512516b18d
Adriana Leticya <leticya.gontijo@gmail.com>
2020-06-03 18:28:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   firstPage: 0,
   finalPage: 60,
-  paramPage: '?page=',
-  // paramPage: 'page/',
+  paramPage: '/?page=',
+  // paramPage: '/page/',
   fileName: 'tor.csv',
-  baseURL: 'https://blog.torproject.org/'
+  baseURL: 'https://blog.torproject.org'
 }
